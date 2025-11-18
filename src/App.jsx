d9c0f5c6b9d039fde9b7b33a1ae7a78445ac4804
Bharat Singh <Bharats328@gmail.com>
2025-11-18 23:36:07 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@
 import EventList from "./pages/EventList";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js";
-import Header from "./components/Header";
 
 function App() {
   return (
